ba5ed8fa6de81673c99f3657101bee519d8231db
Benjamin Keller <bkeller@cmsgen.net>
2020-04-11 10:26:05 +0200

    Poser les bases du site

---
+++ inc/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <div class="col-left">
+        <img id="logo" src="project/ressources/images/logo.png">
+    </div>
+    <div class="col-right">
+        <ul>
+            <li>
+                <a href="/project/" target="_blank">Documentation du projet</a>
+            </li>
+            <li>
+                <a href="https://github.com/benk79hes/ria-doc" target="_blank">Github</a>
+            </li>
+            <li>
+                <a href="https://tree.taiga.io/project/benk79-ria-mmb/backlog" target="_blank">Gestion du projet</a>
+            </li>
+        </ul>
+    </div>
+</footer>
